7ea6a9f60fd00ff4811806cd203d82d0ac29a83e
Panachai <panachai.ny@gmail.com>
2022-11-26 17:29:19 +0700

    add

--- pages/index.vue
+++ pages/index.vue
@@ -12,16 +12,44 @@
       ></pdf> -->
 
       <iframe
-        src="https://cdn.rawgit.com/mozilla/pdf.js/c6e8ca86/test/pdfs/freeculture.pdf#toolbar=0"
+        id="scrollDive"
+        :src="src"
         width="100%"
         height="500px"
         frameborder="0"
+        @scroll="test"
       >
       </iframe>
 
       <p>this 3</p>
 
-      <a href="/assets/pdfs/example.pdf" download="">Download</a>
+      <!-- <a href="/assets/pdfs/example.pdf" download="">Download</a> -->
+
+      <p>this 4</p>
+
+      <!-- <div
+        contenteditable="true"
+        id="scrollDive"
+        class="scroll-div"
+        @scroll="test"
+      >
+        x x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+        x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x xx x x x x x
+      </div> -->
     </v-col>
   </v-row>
 </template>
@@ -29,25 +57,38 @@
 <script>
 import pdf from 'vue-pdf'
 
-// var loadingTask = pdf.createLoadingTask(
-//   'https://cdn.rawgit.com/mozilla/pdf.js/c6e8ca86/test/pdfs/freeculture.pdf'
-// )
-
 export default {
   name: 'IndexPage',
   components: {
     pdf,
   },
-  // data() {
-  //   return {
-  //     show: true,
-  //     src: 'https://cdn.rawgit.com/mozilla/pdf.js/c6e8ca86/test/pdfs/freeculture.pdf',
-  //     loadedRatio: 0,
-  //     page: 1,
-  //     numPages: 0,
-  //     rotate: 0,
-  //   }
-  // },
+  data() {
+    return {
+      src:
+        'https://cdn.rawgit.com/mozilla/pdf.js/c6e8ca86/test/pdfs/freeculture.pdf' +
+        '#toolbar=0',
+      // src: require('@/assets/pdfs/example.pdf') + '#toolbar=0',
+
+      // src: 'https://www.pexels.com/search/tiger',
+    }
+  },
+  methods: {
+    // onScroll({ target: { scrollTop, clientHeight, scrollHeight } }) {
+    //   print('=========', scrollTop, clientHeight, scrollHeight)
+    //   if (scrollTop + clientHeight >= scrollHeight) {
+    //     // this.loadMorePosts()
+    //     print('=========')
+    //   }
+    // },
+    test(e) {
+      // console.log('scroll', this.$refs.scrollHeight)
+      // console.log('scroll', this.$refs.scrollHeight)
+
+      console.log('1', e)
+      console.log('this.value = window.scrollY;', (this.value = window.scrollY))
+      // scrollDive
+    },
+  },
 
   // data() {
   //   return {
@@ -69,3 +110,11 @@ export default {
   // },
 }
 </script>
+<style scoped>
+.scroll-div {
+  margin-top: 20px;
+  border: 1px solid;
+  height: 60px;
+  overflow: auto;
+}
+</style>
